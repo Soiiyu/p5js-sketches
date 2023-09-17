b986492f96d1119d01a07fff4ffe766458aa41ea
Soiiyu <soiiyukoto@gmail.com>
2023-09-18 01:36:08 +0300

    Added sliders

--- projects/osu Pattern Exporter/sketch.js
+++ projects/osu Pattern Exporter/sketch.js
@@ -1,5 +1,5 @@
-let input, map, hitobjects, cs, start, end, colorOffset, newCombo, gradiant, approachSwitch, followPoint, g1, g2, approachOpacity;
-let hitcircle, overlay, numbers, approach;
+let input, map, hitobjects, cs, start, end, loadMap, colorOffset, newCombo, gradiant, approachSwitch, followPoint, g1, g2, approachOpacity;
+let hitcircle, overlay, numbers, approach, sliderCanvas;
 let colors = [
   [255, 242, 140],
   [101, 252, 171],
@@ -94,6 +94,20 @@ function draw() {
   activeObjects.forEach((obj, i) => {
     let combo = findCombo(newCombo, obj.time);
     let ratio = i / (activeObjects.length - 1);
+    let circleAlpha = round(ratio * 128 + 127);
+
+    // Draw slider
+    if (obj.type == 'slider') {
+      sliderCanvas = createGraphics(width, height)
+
+      sliderCanvas.stroke(82, 84, 128)
+      drawSlider(obj.sliderTicks, r)
+      sliderCanvas.stroke(25, 22, 30)
+      drawSlider(obj.sliderTicks, r * 0.885)
+
+      tint(255, circleAlpha * 0.75);
+      image(sliderCanvas, width * 0.5, height * 0.5)
+    }
 
     // Calculate the combo number as a string
     let comboStr = obj.combo.toString();
@@ -111,7 +125,6 @@ function draw() {
     let startX = obj.x - totalWidth / 2;
 
     let c = gradiant.checked() ? lerpColor(g1.color(), g2.color(), ratio) : color(...colors[combo.color])
-    let circleAlpha = round(ratio * 128 + 127);
 
     tint(red(c), green(c), blue(c), circleAlpha);
     image(hitcircle, obj.x, obj.y, r, r);
@@ -144,6 +157,8 @@ function handleFile(file) {
       .toString()
       .split("CircleSize:")[1]
   );
+
+  //x,y,time,type,_,sliderTicks,1,90
   // getting all hitobjects
   let counter = 1;
   hitobjects = map
@@ -152,12 +167,15 @@ function handleFile(file) {
     .split("\n")
     .filter((l) => l !== "")
     .map((l) => {
-      let [x, y, time, type] = l.split(",");
+      let [x, y, time, type, _, sliderTicks] = l.split(",");
       [x, y, time] = [x, y, time].map((n) => parseInt(n));
       let newCombo = (type & 4) == 4;
       if ((type & 1) == 1) type = "circle";
       else if ((type & 8) == 8) type = "spinner";
-      else if ((type & 2) == 2) type = "slider";
+      else if ((type & 2) == 2) {
+        type = "slider";
+        sliderTicks = `${x}:${y}|${sliderTicks.substring(2)}`
+      }
 
       if (newCombo) counter = 1;
       let combo = counter
@@ -166,7 +184,7 @@ function handleFile(file) {
       y += approachMax / 2;
 
       counter++
-      return { x, y, time, type, combo, newCombo };
+      return { x, y, time, type, combo, newCombo, sliderTicks };
     });
   newCombo = hitobjects.reduce(
     (acc, curr, i) => {
@@ -186,11 +204,18 @@ function handleFile(file) {
     { arr: [], spinner: false, color: 1 }
   ).arr;
   console.log(newCombo);
-  start = createInput("0");
-  end = createInput(hitobjects[hitobjects.length - 1].time.toString());
-  start.input(draw);
-  end.input(draw);
-  draw();
+  if (!start) {
+    start = createInput('0');
+    end = createInput(hitobjects[hitobjects.length - 1].time.toString());
+    loadMap = createButton('Load Section')
+    loadMap.mousePressed(draw)
+  } else {
+    start.value('0')
+    end.value(hitobjects[hitobjects.length - 1].time.toString())
+  }
+  // start.input(draw);
+  // end.input(draw);
+  // draw();
   console.log(hitobjects);
 }
 
@@ -201,4 +226,61 @@ function findCombo(newCombo, time) {
     if (time >= newCombo[i].time) break;
   }
   return out;
+}
+
+// Draw sliders
+let lerp2 = (t, p1, p2) => (1 - t) * p1 + t * p2;
+let reduce = (t, p1, p2, ...ps) => ps.length > 0 ? [lerp2(t, p1, p2), ...reduce(t, p2, ...ps)] : [lerp2(t, p1, p2)];
+let deCasteljau = (t, ps) => ps.length > 1 ? deCasteljau(t, reduce(t, ...ps)) : ps[0];
+
+function drawBezier(x, y) {
+  // console.log(x, y)
+
+  // Set the starting point for the bezier curve
+  let x1 = x[0];
+  let y1 = y[0];
+  moveTo(x1, y1);
+
+  // Loop through the points and draw the bezier curve
+  let distance = 0
+  for (let t = 0; t <= 1; t += 0.01) {
+
+    let x2 = deCasteljau(t, x);
+    let y2 = deCasteljau(t, y);
+    sliderCanvas.line(x1, y1, x2, y2);
+
+    distance += dist(x1, y1, x2, y2)
+    x1 = x2;
+    y1 = y2;
+  }
+  return distance
+}
+
+function drawSlider(pointsString, radius) {
+  sliderCanvas.strokeWeight(radius)
+
+  let points = pointsString.split('|')
+  let slices = []
+
+  let currentSlice = []
+  for (let p of points) {
+    // if(currentSlice.length > 0) console.log(currentSlice[currentSlice.length - 1], p)
+    if (currentSlice.length > 0 && currentSlice[currentSlice.length - 1] == p) {
+      // console.log('new slice')
+      slices.push(currentSlice)
+      currentSlice = []
+    }
+    currentSlice.push(p)
+  }
+  // offset by approachMax
+  slices = [...slices.map(p => p.map(pp => pp.split(':').map(n => parseFloat(n)))), currentSlice.map(p => p.split(':').map(n => parseFloat(n) + (approachMax / 2)))]
+
+  let distance = 0
+  for (let slice of slices) {
+    let x = slice.map(p => p[0])
+    let y = slice.map(p => p[1])
+    distance += drawBezier(x, y)
+  }
+  // console.log(slices)
+  console.log(distance)
 }
